8eca416792f5ee2cd51d82a2161c16f175b049e5
Prabin Adhikari <adhikari.prabin10@gmail.com>
2021-03-02 07:52:03 -0500

    change terraform destroy to terraform apply

--- scripts/terraform_apply.sh
+++ scripts/terraform_apply.sh
@@ -2,4 +2,4 @@ cd terraform
 terraform init
 export TF_VAR_git_hash=$(git rev-parse HEAD)
 export TF_VAR_s3_bucket_repository='REPLACE_THIS_BY_ACTUAL_BUCKET'
-terraform destroy -var-file=dev.tfvars --auto-approve
+terraform apply -var-file=dev.tfvars --auto-approve
